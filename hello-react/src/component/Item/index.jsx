9731da8f47f9efdd52654f7c15e9a0f691dbc48a
FeyeW <412755014@qq.com>
2022-08-03 15:29:54 +0800

    feat: 初始化TodoList

---
+++ hello-react/src/component/Item/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.less'
+
+export default class Item extends React.Component {
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
---
+++ hello-react/src/component/List/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.less'
+
+export default class List extends React.Component {
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
