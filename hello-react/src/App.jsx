0c9c9cf5aab8f3cf13baa3078b2b3121221741cb
FeyeW <412755014@qq.com>
2022-08-05 16:05:24 +0800

    feat: 动态初始化列表

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -5,12 +5,20 @@ import Footer from './component/Footer'
 import './App.css'
 
 export default class App extends React.Component {
+    state = {
+        todos: [
+            { id: '001', name: '吃饭', done: true },
+            { id: '002', name: '睡觉', done: true },
+            { id: '003', name: '打代码', done: false }
+        ]
+    }
     render() {
+        const { todos } = this.state
         return (
             <div className="todo-container">
                 <div className="todo-wrap">
                     <Header />
-                    <List />
+                    <List todos={todos} />
                     <Footer />
                 </div>
             </div>
--- hello-react/src/component/Item/index.jsx
+++ hello-react/src/component/Item/index.jsx
@@ -3,11 +3,13 @@ import './index.css'
 
 export default class Item extends React.Component {
   render() {
+    const { name, done } = this.props
+    console.log(done)
     return (
       <li>
         <label>
-          <input type="checkbox" />
-          <span>yyyy</span>
+          <input type="checkbox" defaultChecked={done} />
+          <span>{name}</span>
         </label>
         <button className="btn btn-danger" tyle={{ display: 'none' }}>删除</button>
       </li>
