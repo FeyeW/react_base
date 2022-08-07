f232fbc014ef61022067ef2e6f930f340993dfff
FeyeW <412755014@qq.com>
2022-08-07 12:19:06 +0800

    feat: 初始化github的Demo

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,70 +1,49 @@
 import React from 'react'
-import Header from './component/Header'
-import List from './component/List'
-import Footer from './component/Footer'
 import './App.css'
 
 export default class App extends React.Component {
-    state = {
-        todos: [
-            { id: '001', name: '吃饭', done: true },
-            { id: '002', name: '睡觉', done: true },
-            { id: '003', name: '打代码', done: false }
-        ]
-    }
-    //添加一个函数，用于接收List子组件传递数据
-    addTodo = (todoObj) => {
-        const { todos } = this.state
-        const newTodos = [todoObj, ...todos]
-        this.setState({ todos: newTodos, ...todos })
-    }
-    //添加一个函数，用于接收Item组件的数据
-    updateTodo = (id, done) => {
-        const { todos } = this.state
-        const newTodos = todos.map(todo => {
-            if (todo.id === id) {
-                return { ...todo, done: done }
-            }
-            else return todo
-        })
-        this.setState({ todos: newTodos })
-    }
-
-    //用于删除一个todo对象
-    deleteTodo = (id) => {
-        const { todos } = this.state
-        const newTodos = todos.filter((todoObj) => {
-            return todoObj.id !== id
-        })
-        this.setState({ todos: newTodos })
-    }
-    //用于全选
-    checkAllTodo = (done) => {
-        const { todos } = this.state
-        const newTodos = todos.map((todoObj) => {
-            return { ...todoObj, done: done }
-        })
-        this.setState({ todos: newTodos })
-    }
-    //用于清除所有
-    clearAllDone = () => {
-        const { todos } = this.state
-        const newTodos = todos.filter((todoObj) => {
-            return !todoObj.done
-        })
-        this.setState({ todos: newTodos })
-    }
     render() {
-        const { todos } = this.state
         return (
-            <div className="todo-container">
-                <div className="todo-wrap">
-                    <Header addTodo={this.addTodo} />
-                    <List todos={todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
-                    <Footer todos={todos} checkAllTodo={this.checkAllTodo} clearAllDone={this.clearAllDone} />
+            <div className="container">
+                <section className="jumbotron">
+                    <h3 className="jumbotron-heading">Search Github Users</h3>
+                    <div>
+                        <input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
+                    </div>
+                </section>
+                <div className="row">
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
                 </div>
             </div>
         )
     }
 }
-
