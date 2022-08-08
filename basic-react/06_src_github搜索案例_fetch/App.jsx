a1d9129e9b1f09c6f8c24c14a90a8f666add7955
FeyeW <412755014@qq.com>
2022-08-08 16:39:42 +0800

    feat: 增加fetch发起请求内容

---
+++ basic-react/06_src_github搜索案例_fetch/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import List from './component/List'
+import Sreach from './component/Search'
+
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <Sreach />
+                <List />
+            </div>
+        )
+    }
+}
