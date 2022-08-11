20f758e9dcf4d703f4ea978c63c50da7849f8011
FeyeW <412755014@qq.com>
2022-08-11 20:49:16 +0800

    feat: 编码式路由

---
+++ basic-react/15_src_编码式路由/pages/Home/Message/demo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ComponentName extends Component {
+    render() {
+        return (
+            <div>
+                123sadada
+            </div>
+        )
+    }
+}
+export default ComponentName;
