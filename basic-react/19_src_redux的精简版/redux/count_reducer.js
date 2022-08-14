a2e84b5e27a8056049144d778b4e1bbe2ac6a0ec
FeyeW <412755014@qq.com>
2022-08-14 15:46:01 +0800

    feat: redux的精简版

---
+++ basic-react/19_src_redux的精简版/redux/count_reducer.js
@@ -0,0 +1,19 @@
+/* 
+1.该文件是用于创建一个Count组件服务的reducer，reducer的本质就是一个函数
+2.reducer函数会接到两个参数，分别为：之前的状态（preState）和动作对象（action）
+*/
+
+export default function countRducer(preState, action) {
+    //从state对象中获取对象：type、data
+    const { type, data } = action
+    //根据type决定如何加工
+    switch (type) {
+        case 'increment':
+            return preState + data
+        case 'decrement':
+            return preState - data
+
+        default:
+            return 0
+    }
+}
